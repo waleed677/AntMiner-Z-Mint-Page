321dd0ec73c39eb2c8f2b2d4832ee53cde23b305
waleed677 <waleedmalik677@gmail.com>
2023-07-29 18:42:46 +0500

    add new WL address

--- src/pages/HomePage/Home.js
+++ src/pages/HomePage/Home.js
@@ -17,7 +17,7 @@ const keccak256 = require('keccak256');
 const leafNodes = whitelistAddresses.map(addr => keccak256(addr));
 const merkleTree = new MerkleTree(leafNodes, keccak256, { sortPairs: true });
 const rootHash = merkleTree.getRoot();
-console.log('Whitelist Merkle Tree\n', merkleTree.toString());
+console.log('Merkle Tree\n', merkleTree.toString());
 
 
 function Home() {
