a024c351fe1e8c48b57aac14ff8da4b9d821b99b
Waleed Arshad Awan <waleedmalik677@gmail.com>
2023-11-13 03:40:44 +0500

    add new contract

--- src/pages/HomePage/Home.js
+++ src/pages/HomePage/Home.js
@@ -334,7 +334,7 @@ function Home() {
           <s.FlexContainer fd={"row"} ai={"center"} jc={"space-between"}>
             <s.TextTitle>Available</s.TextTitle>
             <s.TextTitle color={"var(--primary)"}>
-              {CONFIG.MAX_SUPPLY - supply} / 500
+              {CONFIG.MAX_SUPPLY - supply} / 50
             </s.TextTitle>
           </s.FlexContainer>
           <s.SpacerSmall />
