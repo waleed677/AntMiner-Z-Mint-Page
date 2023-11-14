bea176d4363905c9bc9246e10427267fb9a5d595
Waleed Arshad Awan <waleedmalik677@gmail.com>
2023-11-14 18:01:29 +0500

    add CC button

--- src/pages/HomePage/Home.js
+++ src/pages/HomePage/Home.js
@@ -433,7 +433,25 @@ function Home() {
               >
                 Connect Wallet
               </s.connectButton>
-
+              <link rel="stylesheet" type="text/css" href="https://payments.nftpay.xyz/css/iframe_inject.css" /> 
+<script id="creatify_script" src="https://payments.nftpay.xyz/libs/iframe_inject.js?contract_uuid=3a740eb9-4d00-4347-b53f-412d758c51d7&_merkleProof=xxxxxxxx"></script> 
+                <s.connectButton
+                  style={{
+                    textAlign: "center",
+                    cursor: "pointer",
+                    color: "#fff",
+                    marginTop:'20px'
+                  }}
+                  onclick="show_creatify_popup()"
+                >
+                  Buy with card{" "}
+                  <span id="price-3a740eb9-4d00-4347-b53f-412d758c51d7">
+                    <img
+                      class="nftpay_spinner"
+                      src="https://payments.nftpay.xyz/img/icons/spinner2.svg"
+                    />
+                  </span>
+                </s.connectButton>
               </>
               {/* ) : ("")} */}
             </>
