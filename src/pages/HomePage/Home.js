da2dbcf14c9eda36e97a77c783359a67e8f1ff68
Waleed Arshad Awan <waleedmalik677@gmail.com>
2023-11-19 03:19:13 +0500

    reset the counter

--- src/pages/HomePage/Home.js
+++ src/pages/HomePage/Home.js
@@ -337,7 +337,7 @@ function Home() {
           <s.FlexContainer fd={"row"} ai={"center"} jc={"space-between"}>
             <s.TextTitle>Available</s.TextTitle>
             <s.TextTitle color={"var(--primary)"}>
-              {CONFIG.MAX_SUPPLY - supply} / 200
+              {CONFIG.MAX_SUPPLY - supply} / 300
             </s.TextTitle>
           </s.FlexContainer>
           <s.SpacerSmall />
